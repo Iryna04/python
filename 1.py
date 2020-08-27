d15f25f88fd488f0cd675c73e103f03f726509bb
Iryna <irysia2912@gmail.com>
2020-08-27 16:57:49 +0300

    homework-1

---
+++ 1.py
@@ -0,0 +1,11 @@
+a = 1
+b = 2
+c = 3
+print(a, b, c)
+
+name = input('Введите имя: ')
+surname = input('Введите фамилию : ')
+day = int(input('Введите день рождения цифрой: '))
+month = int(input('Введите месяц рождения цифрой: '))
+year = int(input('Введите год рождения цифрой: '))
+print(f'Имя: {name} Фамилия: {surname} День рождения: {day}.{month}.{year}')
